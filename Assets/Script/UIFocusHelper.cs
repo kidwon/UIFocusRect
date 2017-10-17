34d6307a3afcfb79a32213fbccfc143d54e4fa20
kidwon <kidwon@gamil.com>
2017-10-17 19:51:46 +0800

    first commit

---
+++ Assets/Script/UIFocusHelper.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// UI Focus动画
+/// </summary>
+public class UIFocusHelper : MonoBehaviour
+{
+    public Image target;
+
+
+    private Vector4 center;
+    private Material _material;
+    private float diameter; // 直径
+    //private float current = 0f;
+
+    //UI Rect宽度的一半
+    private float offsetX = 0f;
+    //UI Rect高度的一半
+    private float offsetY = 0f;
+    //当前遮挡宽度
+    private float currentX = 0f;
+    //当前遮挡高度
+    private float currentY = 0f;
+
+    Vector3[] corners = new Vector3[4];
+    Canvas _canvas;
+
+    public Canvas canvas
+    {
+        get
+        {
+            if (_canvas == null)
+            {
+                _canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+            }
+            return _canvas;
+        }
+    }
+
+    public Camera UICamera
+    {
+        get
+        {
+            return canvas.GetComponent<Camera>();
+        }
+    }
+    [SerializeField]
+    Image _coverImage;
+
+    private void Start()
+    {
+        FocusOn(target.GetComponent<RectTransform>());
+    }
+
+    public void FocusOn(RectTransform targetRectTransform)
+    {
+        targetRectTransform.GetWorldCorners(corners);
+        //diameter = Vector2.Distance(WordToCanvasPos(canvas, corners[0]), WordToCanvasPos(canvas, corners[2])) / 2f;
+        offsetX = Vector2.Distance(WordToCanvasPos(canvas, corners[0]), WordToCanvasPos(canvas, corners[3])) / 2f;
+        offsetY = Vector2.Distance(WordToCanvasPos(canvas, corners[0]), WordToCanvasPos(canvas, corners[1])) / 2f;
+        float x = corners[0].x + ((corners[3].x - corners[0].x) / 2f);
+        float y = corners[0].y + ((corners[1].y - corners[0].y) / 2f);
+
+        Vector3 center = new Vector3(x, y, 0f);
+        Vector2 position = Vector2.zero;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, center, UICamera, out position);
+
+        center = new Vector4(position.x, position.y, 0f, 0f);
+
+        _material = _coverImage.material;
+
+        _material.SetVector("_Center", center);
+
+        (canvas.transform as RectTransform).GetWorldCorners(corners);
+
+        for (int i = 0; i < corners.Length; i++)
+        {
+            if(i % 2 == 0)
+            {
+                currentX = Mathf.Max(Vector3.Distance(WordToCanvasPos(canvas, corners[i]), center), currentX);
+            }
+            else
+            {
+                currentY = Mathf.Max(Vector3.Distance(WordToCanvasPos(canvas, corners[i]), center), currentY);
+            }
+        }
+
+        _material.SetFloat("_SliderX", currentX);
+        _material.SetFloat("_SliderY", currentY);
+
+    }
+
+
+    float yVelocity = 0f;
+    float xVelocity = 0f;
+    void Update()
+    {
+        //float value = Mathf.SmoothDamp(current, diameter, ref yVelocity, 0.3f);
+        //if (!Mathf.Approximately(value, current))
+        //{
+        //    current = value;
+        //    _material.SetFloat("_Slider", current);
+        //}
+        float valueX = Mathf.SmoothDamp(currentX, offsetX, ref xVelocity, 0.3f);
+        float valueY = Mathf.SmoothDamp(currentY, offsetY, ref yVelocity, 0.3f);
+        if (!Mathf.Approximately(valueX, currentX))
+        {
+            currentX = valueX;
+            _material.SetFloat("_SliderX", currentX);
+        }
+        if (!Mathf.Approximately(valueY, currentY))
+        {
+            currentY = valueY;
+            _material.SetFloat("_SliderY", currentY);
+        }
+
+    }
+
+    static Vector2 WordToCanvasPos(Canvas canvas, Vector3 world)
+    {
+        Vector2 position = Vector2.zero;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, world, canvas.GetComponent<Camera>(), out position);
+        return position;
+    }
+}
+
---
+++ Assets/Shader/Default_Mask.shader
@@ -0,0 +1,142 @@
+﻿Shader "UI/Default_Mask"
+{
+	Properties
+	{
+		[PerRendererData] _MainTex ("Sprite Texture", 2D) = "white" {}
+		_Color ("Tint", Color) = (1,1,1,1)
+		
+		_StencilComp ("Stencil Comparison", Float) = 8
+		_Stencil ("Stencil ID", Float) = 0
+		_StencilOp ("Stencil Operation", Float) = 0
+		_StencilWriteMask ("Stencil Write Mask", Float) = 255
+		_StencilReadMask ("Stencil Read Mask", Float) = 255
+ 
+		_ColorMask ("Color Mask", Float) = 15
+ 
+ 
+		[Toggle(UNITY_UI_ALPHACLIP)] _UseUIAlphaClip ("Use Alpha Clip", Float) = 0
+ 
+ 
+	//-------------------add----------------------
+      _Center("Center", vector) = (0, 0, 0, 0)
+      _SliderX ("_SliderX", Range (0,1000)) = 1000 // sliders
+      _SliderY ("_SliderY", Range (0,1000)) = 1000
+    //-------------------add----------------------
+	}
+ 
+	SubShader
+	{
+		Tags
+		{ 
+			"Queue"="Transparent" 
+			"IgnoreProjector"="True" 
+			"RenderType"="Transparent" 
+			"PreviewType"="Plane"
+			"CanUseSpriteAtlas"="True"
+		}
+		
+		Stencil
+		{
+			Ref [_Stencil]
+			Comp [_StencilComp]
+			Pass [_StencilOp] 
+			ReadMask [_StencilReadMask]
+			WriteMask [_StencilWriteMask]
+		}
+ 
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		ZTest [unity_GUIZTestMode]
+		Blend SrcAlpha OneMinusSrcAlpha
+		ColorMask [_ColorMask]
+ 
+		Pass
+		{
+			Name "Default"
+		CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma target 2.0
+ 
+			#include "UnityCG.cginc"
+			#include "UnityUI.cginc"
+ 
+			#pragma multi_compile __ UNITY_UI_ALPHACLIP
+			
+			struct appdata_t
+			{
+				float4 vertex   : POSITION;
+				float4 color    : COLOR;
+				float2 texcoord : TEXCOORD0;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+ 
+			struct v2f
+			{
+				float4 vertex   : SV_POSITION;
+				fixed4 color    : COLOR;
+				float2 texcoord  : TEXCOORD0;
+				float4 worldPosition : TEXCOORD1;
+				UNITY_VERTEX_OUTPUT_STEREO
+ 
+			};
+			
+			fixed4 _Color;
+			fixed4 _TextureSampleAdd;
+			float4 _ClipRect;
+			//-------------------add----------------------
+            float _SliderX;
+            float _SliderY;
+            float2 _Center;
+            //-------------------add----------------------
+			v2f vert(appdata_t IN)
+			{
+				v2f OUT;
+				UNITY_SETUP_INSTANCE_ID(IN);
+				UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(OUT);
+				OUT.worldPosition = IN.vertex;
+				OUT.vertex = UnityObjectToClipPos(OUT.worldPosition);
+ 
+				OUT.texcoord = IN.texcoord;
+				
+				OUT.color = IN.color * _Color;
+				return OUT;
+			}
+ 
+			sampler2D _MainTex;
+ 
+			fixed4 frag(v2f IN) : SV_Target
+			{
+				half4 color = (tex2D(_MainTex, IN.texcoord) + _TextureSampleAdd) * IN.color;
+				
+				color.a *= UnityGet2DClipping(IN.worldPosition.xy, _ClipRect);
+				
+				#ifdef UNITY_UI_ALPHACLIP
+				clip (color.a - 0.001);
+				#endif
+				//-------------------add----------------------
+               	//color.a*=(distance(IN.worldPosition.xy,_Center.xy) > _Slider);
+                float2 A = _Center.xy + float2(0,_SliderY) + (float2(-_SliderX,0));
+                float2 B = _Center.xy + float2(0,-_SliderY) +(float2(-_SliderX,0));
+                float2 C = _Center.xy + float2(_SliderX,0) +  float2(0,-_SliderY);
+                float2 M = IN.worldPosition.xy;
+                float2 BC = C-B;
+                float2 AB = B-A;
+                float2 BM = M-B;
+                float2 AM = M-A;
+                color.a*=!(dot(AB,AM) >= 0 && dot(AB,AM) <= dot(AB,AB) && dot(BC,BM)>= 0 && dot(BC,BM) <= dot(BC,BC));
+               	color.rgb*= color.a;
+               	//-------------------add----------------------
+				return color;
+			
+			}
+
+            bool pointInRect(float2 A,float2 B,float2 C,float2 M)
+            {
+                
+            }
+		ENDCG
+		}
+	}
+}
